16a93338c6626c63753a59576f52a242ac886d3f
Johannes Herr <joe@faramir>
2014-05-14 21:36:20 +0200

    simplified webserver

--- webserver.js
+++ webserver.js
@@ -1,40 +1,27 @@
 var http = require("http"),
     url = require("url"),
     path = require("path"),
-    fs = require("fs"),
-    port = process.argv[2] || 8888;
+    fs = require("fs");
+
+
+var port = 8888;
 
 http.createServer(function(request, response) {
 
   var uri = url.parse(request.url).pathname,
       filename = path.join(process.cwd(), uri);
 
-  console.log('file:'+ filename );
-
-
-  path.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
-
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-    fs.readFile(filename, "binary", function(err, file) {
+    fs.readFile(filename, function(err, file) {
       if(err) {
         response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
+        response.end(err + "\n");
         return;
+      } else {
+        response.writeHead(200);
+        response.end(file);
       }
-
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
     });
-  });
-}).listen(parseInt(port, 10));
 
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+}).listen(port);
+
+console.log("http://localhost:" + port + "/");
